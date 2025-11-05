c821f99bf086e787d8e45a459bd7cac70a6d8e82
SanicVenture <jthum579@gmail.com>
2025-11-05 16:59:28 -0500

    Beginning of server. Created ParkingLotState struct.

---
+++ parking_availability_server/ParkingAvailabilityServer/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public struct ParkingLotState(bool newvehicle, bool newobstructed)
+{
+    bool vehicle = newvehicle;
+    bool obstructed = newobstructed;
+
+    public bool[] GetAllParkingLotState()
+    {
+        return [vehicle, obstructed];
+    }
+}
+
