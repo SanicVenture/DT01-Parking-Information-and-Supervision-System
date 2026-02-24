8c9cd65f76e6986e2f0adfe55b2b0fbbdf8defce
SanicVenture <jthum579@gmail.com>
2026-02-24 13:35:09 -0500

    Start to using YOLO for proper object detection.

--- parking_availability_server/NewParkingAvailabilityServer/OpenCVManager.cs
+++ parking_availability_server/NewParkingAvailabilityServer/OpenCVManager.cs
@@ -1,11 +1,24 @@
-﻿using NewParkingAvailabilityServer.Models;
+﻿using Microsoft.ML.OnnxRuntime;
+using NewParkingAvailabilityServer.Models;
 using OpenCvSharp;
 using System.Drawing;
 using System.Security.Cryptography.Xml;
 using static System.Net.Mime.MediaTypeNames;
 using System.Data;
 using Humanizer;
-using System.Windows.Forms;
+using SkiaSharp;
+using YoloDotNet;
+using YoloDotNet.Models;
+using YoloDotNet.Extensions;
+using YoloDotNet.ExecutionProvider.Cpu;
+using Compunet.YoloSharp;
+using Compunet.YoloSharp.Plotting;
+using SixLabors.ImageSharp;
+using Compunet.YoloSharp.Data;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+
 
 namespace NewParkingAvailabilityServer
 {
@@ -53,11 +66,12 @@ namespace NewParkingAvailabilityServer
                     return;
                 }
                 //using (var frame = new Mat())
-                using (Mat frame = Cv2.ImRead("input_frame.jpg"))
+                //using (Mat frame = Cv2.ImRead("input_frame.jpg"))
+                using (Mat frame = Cv2.ImRead("input_frame_with_cones.png"))
                 {
                     while (true)
                     {
-                        using (var dest = frame.Clone())
+                        using (var frameClone = frame.Clone())
                         {
                             //Cv2.CvtColor(frame, dest, ColorConversionCodes.BGR2GRAY);
 
@@ -75,6 +89,7 @@ namespace NewParkingAvailabilityServer
                             //START OF LOOP
 
 
+
                             List<OpenCvSharp.Point> points = new List<OpenCvSharp.Point>();
 
                             Cv2.NamedWindow("Select ROI");
@@ -83,15 +98,15 @@ namespace NewParkingAvailabilityServer
                                 if (@event == MouseEventTypes.LButtonDown)
                                 {
                                     points.Add(new OpenCvSharp.Point(x, y));
-                                    Cv2.Circle(dest, new OpenCvSharp.Point(x, y), 3, Scalar.Red, -1);
-                                    Cv2.ImShow("Select ROI", dest);
+                                    Cv2.Circle(frameClone, new OpenCvSharp.Point(x, y), 3, Scalar.Red, -1);
+                                    Cv2.ImShow("Select ROI", frameClone);
                                 }
                             });
 
-                            Cv2.ImShow("Select ROI", dest);
+                            Cv2.ImShow("Select ROI", frameClone);
                             Cv2.WaitKey(0);
 
-                            Mat mask = new Mat(dest.Size(), MatType.CV_8UC1, Scalar.All(0));
+                            Mat mask = new Mat(frameClone.Size(), MatType.CV_8UC1, Scalar.All(0));
 
                             OpenCvSharp.Point[][] polygon;
 
@@ -103,103 +118,171 @@ namespace NewParkingAvailabilityServer
                             }
 
                             Mat dst = new Mat();
-                            Cv2.BitwiseAnd(dest, dest, dst, mask);
+                            Cv2.BitwiseAnd(frameClone, frameClone, dst, mask);
 
                             Cv2.ImShow("ROI", dst);
                             Cv2.WaitKey(0);
 
-                            //END OF LOOP
+                            //instead of what's below, do YOLO algorithm instead. 
 
 
+                            using var predictor = new YoloPredictor("yolov8n.onnx");
 
-                            #region collapsed
+                            var result = await predictor.DetectAsync("input_frame_with_cones.png");
 
-                            //Cv2.GaussianBlur(dest, dest, new OpenCvSharp.Size(5, 5), 0);
+                            Image<Rgba32> image = SixLabors.ImageSharp.Image.Load<Rgba32>("input_frame_with_cones.png");
 
-                            //Cv2.ImShow("Gaussian Blur", dest);
-                            //Cv2.WaitKey(0);
-
-                            //Cv2.Dilate(dest, dest, Mat.Ones(3, 3, MatType.CV_8UC1));
-
-                            //Cv2.ImShow("Dilation", dest);
-                            //Cv2.WaitKey(0);
-
-                            //Mat kernel = Cv2.GetStructuringElement(MorphShapes.Ellipse, new OpenCvSharp.Size(2, 2));
-                            //Cv2.MorphologyEx(dest, dest, MorphTypes.Close, kernel);
-
-                            //Cv2.ImShow("Morphologically Closed Image", dest);
-                            //Cv2.WaitKey(0);
+                            foreach (var detection in result)
+                            {
+                                var rectangle = new SixLabors.ImageSharp.PointF[] { 
+                                    new SixLabors.ImageSharp.Point(detection.Bounds.Bottom, detection.Bounds.Left), 
+                                    new SixLabors.ImageSharp.Point(detection.Bounds.Bottom, detection.Bounds.Right),
+                                    new SixLabors.ImageSharp.Point(detection.Bounds.Right, detection.Bounds.Top),
+                                    new SixLabors.ImageSharp.Point(detection.Bounds.Left, detection.Bounds.Top) };
 
 
-                            //using (var cannyed_image = new Mat())
+                                image.Mutate(ctx => ctx.DrawPolygon(Rgba32.ParseHex("FF0000"), 2, rectangle));
+                            }
+
+                            image.SaveAsPng("output_frame_with_detections.png");
+
+                            Mat final = Cv2.ImRead("output_frame_with_detections.png");
+
+                            Cv2.ImShow("YOLO Detections", final);
+                            Cv2.WaitKey(0);
+
+
+                            //using var yolo = new Yolo(new YoloOptions
                             //{
-                            //    Cv2.Canny(dest, cannyed_image, 100, 200);
 
-                            //    Cv2.ImShow("Cannyed Image", cannyed_image);
-                            //    Cv2.WaitKey(0);
+                            //    ExecutionProvider = new CpuExecutionProvider("yolov8n.onnx"),
 
-                            //    string roiselect = "Select ROI";
+                            //});
 
+                            //using var image = SKBitmap.Decode("input_frame_with_cones.png");
 
+                            //var results = yolo.RunObjectDetection(image);
 
-                            //    Rect roi = Cv2.SelectROI(roiselect, cannyed_image);
-
-                            //    Cv2.DestroyWindow(roiselect);
-
-                            //    using (var cropped_image = new Mat(cannyed_image, roi))
-                            //    {
-                            //        Cv2.ImShow("Cropped Image", cropped_image);
-                            //        Cv2.WaitKey(0);
-
-                            //        LineSegmentPoint[] lines = Cv2.HoughLinesP(cropped_image, 6, Math.PI / 60, 80, 40, 25);
-
-                            //        //using (var cropped_original = (new Mat(frame, roi)).Clone())
-                            //        //{ 
+                            //image.Draw(results);
 
 
-                            //        //}
+                            //end of yolo
 
 
+                            Mat greydst = new Mat();
 
+                            Cv2.CvtColor(dst.Clone(), greydst, ColorConversionCodes.BGR2GRAY);
 
-                            //        ////image = new Mat(image);
-                            //        //Mat line_image = new Mat(cropped_original.Size(), cropped_original.Type());
-                            //        //foreach (LineSegmentPoint line in lines)
-                            //        //{
-                            //        //    Cv2.Line(line_image, line.P1.X, line.P1.Y, line.P2.X, line.P2.Y, Scalar.Red, 3);
-                            //        //}
-                            //        //Cv2.AddWeighted(image, 0.8, line_image, 1.0, 0.0, image);
+                            Cv2.ImShow("Grayscale Image", greydst);
+                            Cv2.WaitKey(0);
 
-                            //        //return image;
+                            int pixels = Cv2.CountNonZero(greydst);
 
-                            //        using (var lined_image = DrawLines((new Mat(frame, roi)).Clone(), lines, Scalar.Red))
-                            //        {
-                            //            Cv2.ImShow("Lined Image", lined_image);
-                            //            Cv2.WaitKey(0);
-                            //        }
+                            using (var cannyed_image = new Mat())
+                            {
+                                Cv2.Canny(dst, cannyed_image, 100, 200);
 
-                            //        lines = lines.OrderBy(x => x.P1.X).ToArray();
+                                Cv2.ImShow("Cannyed Image", cannyed_image);
+                                Cv2.WaitKey(0);
 
-                            //        //foreach (var line in lines)
-                            //        //{
-                            //        //    using (var line_image = DrawLines((new Mat(frame, roi)).Clone(), [line], Scalar.Red))
-                            //        //    {
-                            //        //        Cv2.ImShow("Lined Image", line_image);
-                            //        //        Cv2.WaitKey(0);
-                            //        //    }
-                            //        //}
+                                int cannypixels = Cv2.CountNonZero(cannyed_image);
 
 
 
-                            //    }
+                            }
+
+                                //END OF LOOP
 
 
 
-                            //}
+                                #region collapsed
 
-                            #endregion
+                                //Cv2.GaussianBlur(dest, dest, new OpenCvSharp.Size(5, 5), 0);
 
-                        }
+                                //Cv2.ImShow("Gaussian Blur", dest);
+                                //Cv2.WaitKey(0);
+
+                                //Cv2.Dilate(dest, dest, Mat.Ones(3, 3, MatType.CV_8UC1));
+
+                                //Cv2.ImShow("Dilation", dest);
+                                //Cv2.WaitKey(0);
+
+                                //Mat kernel = Cv2.GetStructuringElement(MorphShapes.Ellipse, new OpenCvSharp.Size(2, 2));
+                                //Cv2.MorphologyEx(dest, dest, MorphTypes.Close, kernel);
+
+                                //Cv2.ImShow("Morphologically Closed Image", dest);
+                                //Cv2.WaitKey(0);
+
+
+                                //using (var cannyed_image = new Mat())
+                                //{
+                                //    Cv2.Canny(dest, cannyed_image, 100, 200);
+
+                                //    Cv2.ImShow("Cannyed Image", cannyed_image);
+                                //    Cv2.WaitKey(0);
+
+                                //    string roiselect = "Select ROI";
+
+
+
+                                //    Rect roi = Cv2.SelectROI(roiselect, cannyed_image);
+
+                                //    Cv2.DestroyWindow(roiselect);
+
+                                //    using (var cropped_image = new Mat(cannyed_image, roi))
+                                //    {
+                                //        Cv2.ImShow("Cropped Image", cropped_image);
+                                //        Cv2.WaitKey(0);
+
+                                //        LineSegmentPoint[] lines = Cv2.HoughLinesP(cropped_image, 6, Math.PI / 60, 80, 40, 25);
+
+                                //        //using (var cropped_original = (new Mat(frame, roi)).Clone())
+                                //        //{ 
+
+
+                                //        //}
+
+
+
+
+                                //        ////image = new Mat(image);
+                                //        //Mat line_image = new Mat(cropped_original.Size(), cropped_original.Type());
+                                //        //foreach (LineSegmentPoint line in lines)
+                                //        //{
+                                //        //    Cv2.Line(line_image, line.P1.X, line.P1.Y, line.P2.X, line.P2.Y, Scalar.Red, 3);
+                                //        //}
+                                //        //Cv2.AddWeighted(image, 0.8, line_image, 1.0, 0.0, image);
+
+                                //        //return image;
+
+                                //        using (var lined_image = DrawLines((new Mat(frame, roi)).Clone(), lines, Scalar.Red))
+                                //        {
+                                //            Cv2.ImShow("Lined Image", lined_image);
+                                //            Cv2.WaitKey(0);
+                                //        }
+
+                                //        lines = lines.OrderBy(x => x.P1.X).ToArray();
+
+                                //        //foreach (var line in lines)
+                                //        //{
+                                //        //    using (var line_image = DrawLines((new Mat(frame, roi)).Clone(), [line], Scalar.Red))
+                                //        //    {
+                                //        //        Cv2.ImShow("Lined Image", line_image);
+                                //        //        Cv2.WaitKey(0);
+                                //        //    }
+                                //        //}
+
+
+
+                                //    }
+
+
+
+                                //}
+
+                                #endregion
+
+                            }
                         //KEEP BELOW. Actual Code
                         //capture.Read(frame);
 
